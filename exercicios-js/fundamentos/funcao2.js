2a72a4321dfb7c8f66c5c07f68186d77d51b1ce0
EduardoRanzzani <eduranzzani@gmail.com>
2019-03-26 20:20:56 -0400

    Commit Inicial
    Primeiro Commit de todas as aulas ja feitas até a data atual

---
+++ exercicios-js/fundamentos/funcao2.js
@@ -0,0 +1,20 @@
+// Armazendnao uma funcao em uma variável
+const imprimirSoma = function (a, b){
+    console.log(a + b)
+}
+
+imprimirSoma(2, 3)
+
+// Armazenando uma funcao arrow em uma variavel
+const soma = (a, b) => {
+    return a + b
+}
+
+console.log(soma(2, 3))
+
+// retorno implicito
+const subtracao = (a, b) => a - b
+console.log(subtracao(2, 3))
+
+const imprimir2 = a => console.log(a)
+imprimir2('Legal!!!')
