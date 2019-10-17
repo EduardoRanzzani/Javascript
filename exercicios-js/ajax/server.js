139ca654d1e36d9cc9b707f8bb457f8c9c0aa2b4
Eduardo Leite Ranzzani <eduranzzani@gmail.com>
2019-10-17 17:34:19 -0400

    aulas escritorio

--- exercicios-js/ajax/server.js
+++ exercicios-js/ajax/server.js
@@ -6,5 +6,34 @@ app.use(express.static('.'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, './upload')
+    },
+    filename: function(req, file, callback) {
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
+const upload = multer({storage}).single('arquivo')
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if (err) {
+            return res.end('Ocorreu um erro.')
+        }
+        res.end('Concluido com sucesso.')
+    })
+})
+
+app.post('/formulario', (req, res) => {
+    res.send({
+        ...res.body,
+        id: 1
+    })
+})
+
 app.get('/teste', (req, res) => res.send('OK'))
-app.listen(8080, () => console.log('Executando...'))
+app.listen(8081, () => console.log('Executando...'))
