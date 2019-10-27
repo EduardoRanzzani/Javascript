a8308ed9950fd21f67245b579b007bd446c88bfb
Eduardo Leite Ranzzani <eduranzzani@gmail.com>
2019-10-26 23:37:15 -0400

    finalização do modulo ajax

--- exercicios-js/ajax/server.js
+++ exercicios-js/ajax/server.js
@@ -12,28 +12,38 @@ const storage = multer.diskStorage({
     destination: function (req, file, callback) {
         callback(null, './upload')
     },
-    filename: function(req, file, callback) {
+    filename: function (req, file, callback) {
         callback(null, `${Date.now()}_${file.originalname}`)
     }
 })
 
-const upload = multer({storage}).single('arquivo')
+const upload = multer({ storage }).single('arquivo')
 
 app.post('/upload', (req, res) => {
     upload(req, res, err => {
         if (err) {
             return res.end('Ocorreu um erro.')
         }
-        res.end('Concluido com sucesso.')
+
+        res.end('Concluído com sucesso.')
     })
 })
 
 app.post('/formulario', (req, res) => {
     res.send({
-        ...res.body,
-        id: 1
+        ...req.body,
+        id: 7
     })
 })
 
-app.get('/teste', (req, res) => res.send('OK'))
-app.listen(8081, () => console.log('Executando...'))
+app.get('/parOuImpar', (req, res) => {
+    // req.body
+    // req.query
+    // req.params
+    const par = parseInt(req.query.numero) % 2 === 0
+    res.send({
+        resultado: par ? 'par' : 'impar'
+    })
+})
+
+app.listen(8080, () => console.log('Executando...'))
